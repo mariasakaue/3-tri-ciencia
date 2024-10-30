24f7bcbd5a2cf5a6c38b3764c3f8503c10af1616
mariasakaue <106112130+mariasakaue@users.noreply.github.com>
2024-10-30 08:13:51 -0300

    Add files via upload

--- 3-trimestre-ciencia-de-dados/grafico/quantidadeUsuarios.js
+++ 3-trimestre-ciencia-de-dados/grafico/quantidadeUsuarios.js
@@ -1,4 +1,4 @@
-import { getCSS } from "./common.js";
+import { getCSS, tickConfig } from "./common.js";
 
 async function quantidadeUsuarios() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
@@ -13,19 +13,50 @@ async function quantidadeUsuarios() {
             y: quantidadeUsuarios,
             type: 'bar',
             marker: {
-                color: getCSS('--secondary-color')
+                color: getCSS('--primary-color')
             }
         }
     ]
 
     const layout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color'),
+        title: {
+            text: 'Redes sociais com mais usuários no mundo',
+            x: 0,
+            font: {
+                color: getCSS('--primary-color'),
+                family: getCSS('--font'),
+                size: 30
+            }
+     
+        },
+        xaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'nome das redes sociais',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+            }
+        },
 
+        yaxis: {
+            tickfont: tickConfig,
+                text: 'bilhões de usuários ativos',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+            }
+        }
+        
+        
+        
+        const grafico = document.createElement('div')
+        grafico.className = 'grafico'
+        document.getElementById('graficos-container').appendChild(grafico)
+        Plotly.newPlot(grafico, data, layout)
     }
-    
-    const grafico = document.createElement('div')
-    grafico.className = 'grafico'
-    document.getElementById('graficos-container').appendChild(grafico)
-    Plotly.newPlot(grafico, data)
-}
+
 
 quantidadeUsuarios()
